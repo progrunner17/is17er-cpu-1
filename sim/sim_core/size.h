546135c84cf85c3d40c2a0da5d12c6dd851ab260
shiatsumat <y.skm24t@gmail.com>
2018-01-11 02:46:03 +0900

    small fix

--- sim/sim_core/size.h
+++ sim/sim_core/size.h
@@ -2,7 +2,7 @@
 #define _SIZE
 
 #define MEMORY_SIZE  (1024 * 1024 * 4)			// == word size == byte size / 4
-#define PROGRAM_SIZE (1024 * 16)
+#define PROGRAM_SIZE (1024 * 1024)
 #define BUF_SIZE (256)
 #define BASE_ADDR 0
 #define MAX_N_WORDS 4096
